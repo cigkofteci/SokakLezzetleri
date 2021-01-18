abf2703bddbe5b92a2daa648a3ddc3285b2eaaa5
cigkofteci <cigkofteci@github.com>
2021-01-18 21:06:01 +0300

    database içecek sipariş kısmı tamamlandı Nuget paket manager sqlite eklendi db bağlantı kurulmaya hazır

--- SokakLezzetleri/icecekSatis.Designer.cs
+++ SokakLezzetleri/icecekSatis.Designer.cs
@@ -36,7 +36,7 @@ namespace SokakLezzetleri
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSepetEkle = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -45,36 +45,39 @@ namespace SokakLezzetleri
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 394);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 485);
             this.dataGridView1.TabIndex = 1;
             // 
             // tBoxOdenenFiyat
             // 
             this.tBoxOdenenFiyat.Font = new System.Drawing.Font("Verdana", 17F, System.Drawing.FontStyle.Bold);
-            this.tBoxOdenenFiyat.Location = new System.Drawing.Point(471, 201);
-            this.tBoxOdenenFiyat.Margin = new System.Windows.Forms.Padding(2);
+            this.tBoxOdenenFiyat.Location = new System.Drawing.Point(628, 247);
+            this.tBoxOdenenFiyat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tBoxOdenenFiyat.Name = "tBoxOdenenFiyat";
-            this.tBoxOdenenFiyat.Size = new System.Drawing.Size(76, 35);
+            this.tBoxOdenenFiyat.Size = new System.Drawing.Size(100, 42);
             this.tBoxOdenenFiyat.TabIndex = 11;
             // 
             // tBoxAlinanAdet
             // 
             this.tBoxAlinanAdet.Font = new System.Drawing.Font("Verdana", 17F, System.Drawing.FontStyle.Bold);
-            this.tBoxAlinanAdet.Location = new System.Drawing.Point(471, 119);
-            this.tBoxAlinanAdet.Margin = new System.Windows.Forms.Padding(2);
+            this.tBoxAlinanAdet.Location = new System.Drawing.Point(628, 146);
+            this.tBoxAlinanAdet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tBoxAlinanAdet.Name = "tBoxAlinanAdet";
-            this.tBoxAlinanAdet.Size = new System.Drawing.Size(76, 35);
+            this.tBoxAlinanAdet.Size = new System.Drawing.Size(100, 42);
             this.tBoxAlinanAdet.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(258, 12);
+            this.label4.Location = new System.Drawing.Point(375, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(509, 26);
+            this.label4.Size = new System.Drawing.Size(648, 34);
             this.label4.TabIndex = 9;
             this.label4.Text = "ADET FİYATI: DATABASEDEN ALINACAK";
             // 
@@ -82,9 +85,10 @@ namespace SokakLezzetleri
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(380, 123);
+            this.label2.Location = new System.Drawing.Point(507, 151);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 26);
+            this.label2.Size = new System.Drawing.Size(109, 34);
             this.label2.TabIndex = 7;
             this.label2.Text = "ADET:";
             // 
@@ -92,9 +96,10 @@ namespace SokakLezzetleri
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(261, 205);
+            this.label1.Location = new System.Drawing.Point(348, 252);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 26);
+            this.label1.Size = new System.Drawing.Size(262, 34);
             this.label1.TabIndex = 12;
             this.label1.Text = "TOPLAM FİYAT:";
             // 
@@ -102,27 +107,30 @@ namespace SokakLezzetleri
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label3.Location = new System.Drawing.Point(263, 249);
+            this.label3.Location = new System.Drawing.Point(351, 306);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(304, 13);
+            this.label3.Size = new System.Drawing.Size(385, 17);
             this.label3.TabIndex = 13;
             this.label3.Text = "OTOMATİK HESAPLANIR AMA ELLE DE DEĞİŞTİRİLEBİLİR";
             // 
-            // button1
+            // btnSepetEkle
             // 
-            this.button1.Location = new System.Drawing.Point(585, 329);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 77);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSepetEkle.Location = new System.Drawing.Point(780, 405);
+            this.btnSepetEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSepetEkle.Name = "btnSepetEkle";
+            this.btnSepetEkle.Size = new System.Drawing.Size(243, 95);
+            this.btnSepetEkle.TabIndex = 14;
+            this.btnSepetEkle.Text = "Sepete Ekle";
+            this.btnSepetEkle.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(382, 380);
+            this.label5.Location = new System.Drawing.Point(509, 468);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(167, 26);
+            this.label5.Size = new System.Drawing.Size(210, 34);
             this.label5.TabIndex = 15;
             this.label5.Text = "ZAMAN BİLGİSİ DE TUTULMALI\r\n\r\n";
             // 
@@ -130,20 +138,21 @@ namespace SokakLezzetleri
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label6.Location = new System.Drawing.Point(552, 205);
+            this.label6.Location = new System.Drawing.Point(736, 252);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(243, 26);
+            this.label6.Size = new System.Drawing.Size(311, 34);
             this.label6.TabIndex = 16;
             this.label6.Text = "ENABLED = FALSE OLSUN\r\nSADECE FOKUS İKEN ENABLED TRUE OLSUN";
             // 
             // icecekSatis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSepetEkle);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tBoxOdenenFiyat);
@@ -151,6 +160,7 @@ namespace SokakLezzetleri
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "icecekSatis";
             this.Text = "icecekSatis";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -167,7 +177,7 @@ namespace SokakLezzetleri
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSepetEkle;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
     }
